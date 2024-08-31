6043834887477bf189b3a28ca381ce3f792df5ad
divyanshgoyal777 <tonisha24680@gmail.com>
2024-08-31 08:47:11 +0530

    backend

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -222,8 +222,8 @@ router.post("/create-checkout-session", async (req, res) => {
       },
     ],
     mode: "payment",
-    success_url: `${process.env.CLIENT_URL}/success`,
-    cancel_url: `${process.env.CLIENT_URL}/cancel`,
+    success_url: "https://writenest.netlify.app//success",
+    cancel_url: "https://writenest.netlify.app//cancel",
   });
 
   res.json({ id: session.id });
